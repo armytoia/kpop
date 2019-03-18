9049a2df923d0e3d845aef1c5282d1158f8eb66a
armytoia <vitoriaacaldeirinha@gmail.com>
2019-03-18 12:06:00 -0300

    segunda_versao

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-}
+ val1: number = 0;
+ val2: number = 0;
+
+
+  somar():void{
+    let total = Number(this.val1) + Number(this.val2);
+    alert('a soma é'+total);
+
+    if(total%2 = 0 ){
+      alert('o número é par');
+
+    }
+    else{
+    alert('o número é ímpar');
+    }
+    }
+  }
