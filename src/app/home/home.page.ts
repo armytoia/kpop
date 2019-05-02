bf5396e0aaf324c1d5dd694af0e138bd468fcae9
armytoia <vitoriaacaldeirinha@gmail.com>
2019-05-02 10:47:45 -0300

    primeira_versao

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,18 +9,24 @@ export class HomePage {
 
  val1: number = 0;
  val2: number = 0;
+ val3: number = 0;
 
+  verificar():void{
+    let val1 = Number(this.val1);
+    let val2 = Number(this.val2);
+    let val3 = Number(this.val3);
 
-  somar():void{
-    let total = Number(this.val1) + Number(this.val2);
-    alert('a soma é'+total);
-
-    if(total%2 = 0 ){
-      alert('o número é par');
-
+    if(val1 > val2 && val1 > val3 ){
+      alert(val1 + ' é o maior número');
+    }
+    else if(val2 > val1 && val2 > val3){
+      alert(val2+ "é o maior número");
+    }
+    else if(val3 > val1 && val3 > val2){
+      alert(val3 +"é o maior número");
     }
     else{
-    alert('o número é ímpar');
+    alert('os números são iguais');
     }
     }
   }
